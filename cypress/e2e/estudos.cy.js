e537979bde566c598ee54d7501cdb932eea849b0
Pedro Henrique <pedroferreira.bsb@gmail.com>
2023-11-10 03:57:10 -0300

    versão atualizada

---
+++ cypress/e2e/estudos.cy.js
@@ -0,0 +1,8 @@
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function () {
+    beforeEach(function () {
+        cy.visit('htpp://www.google.com')
+
+    })
+})
